92bf6b6e57adfd518c3da407e4b6d304db0e8113
Andrea Zonca <code@andreazonca.com>
2017-10-25 03:32:56 -0700

    delete pods and pvc

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 sudo helm delete --purge jup &
+sudo kubectl delete --all pods --namespace=jup
+sudo kubectl delete --all pvc --namespace=jup
 rm jupyterhub*.tgz
 cd zero-to-jupyterhub-k8s
 python build.py --image-prefix='zonca/k8s-' build --push
